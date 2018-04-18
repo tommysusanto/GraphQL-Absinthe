1d01ad0c87516279c919c198acf680f2bf51bf24
Tommy Susanto <tommy88susanto@gmail.com>
2018-04-18 18:29:12 +1000

    add adapter

--- config/prod.exs
+++ config/prod.exs
@@ -20,7 +20,7 @@ config :graphqlabsinthe, GraphqlabsintheWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
-config :hello, Hello.Repo,
+config :graphqlabsinthe, Graphqlabsinthe.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
