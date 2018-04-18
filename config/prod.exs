38a2c07e1c7b95b07134de1f7cf9d3c98d5f3b7f
Tommy Susanto <tommy88susanto@gmail.com>
2018-04-18 19:28:20 +1000

    fix

--- config/prod.exs
+++ config/prod.exs
@@ -17,6 +17,7 @@ config :graphqlabsinthe, GraphqlabsintheWeb.Endpoint,
   load_from_system_env: true,
   url: [scheme: "https", host: "tommy-graphql-test.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 config :graphqlabsinthe, Graphqlabsinthe.Repo,
